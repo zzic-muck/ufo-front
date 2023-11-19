28bd3217893d8faca127a9766aa78509dcab5fc0
zzicmuck <91383940+zzic-muck@users.noreply.github.com>
2023-11-19 23:19:11 +0900

    ladder 배열 수정

--- components/sadari.js
+++ components/sadari.js
@@ -6,60 +6,58 @@ const windowWidth = Dimensions.get('window').width;
 const Sadari = ({cnt}) => {
     const [positions, setPositions] = useState([]);
     const [horizontalLines, setHorizontalLines] = useState([]);
+    const [columnsWithHorizontalLines, setColumnsWithHorizontalLines] = useState([]);
 
     const imageUriArray = ['https://i.namu.wiki/i/NB_qC6YRjH7hv6elNznBIBOBZ5AwE-PKYEWKcU03aFzGsc60bOt9KLxocyvB01OxAbOG8joW9mgkShFmTaTKsQ.webp'];
+
 
     useEffect(() => {
         const newPositions = Array(cnt).fill().map(() => new Animated.ValueXY({x: 0, y: 0}));
         setPositions(newPositions);
 
-        const newHorizontalLines = Array(cnt).fill().map(() => ({
-            top: Math.random() * 300 - 50, // 세로줄 길이 범위 내에서 랜덤 위치
-            connectedColumn: Math.floor(Math.random() * (cnt - 1)) // 연결된 세로줄 인덱스
-        }));
+        const newHorizontalLines = [];
+        const columnsWithHorizontalLines = Array(cnt).fill().map(() => []);
+
+        for (let i = 0; i < cnt - 1; i++) {
+            // for (let j = 0; j < 3; j++) { // 각 세로선당 3개의 가로선
+            const yPosition = Math.random() * 300 + 100;
+            newHorizontalLines.push({
+                fromColumn: i, toColumn: i + 1, yPosition: yPosition,
+            });
+
+            // 현재 세로선에 시작하는 가로선 정보 추가
+            columnsWithHorizontalLines[i].push({
+                toColumn: i + 1, yPosition: yPosition,
+            });
+
+            // 인접한 세로선에 연결되는 가로선 정보 추가
+            columnsWithHorizontalLines[i + 1].push({
+                fromColumn: i, yPosition: yPosition,
+            });
+            // }
+        }
+
         setHorizontalLines(newHorizontalLines);
+        // columnsWithHorizontalLines에 각 세로줄별 가로줄 정보가 저장됩니다.
+        setColumnsWithHorizontalLines(columnsWithHorizontalLines);
+        console.log(columnsWithHorizontalLines); // 디버깅을 위해 콘솔에 출력
     }, [cnt]);
 
+
     const moveImage = index => {
+        console.log("Clicked image index:", index); // 클릭한 이미지의 인덱스를 콘솔에 표시
+        // 여기서 columnsWithHorizontalLines 사용 가능
+        console.log("Horizontal lines for column", index, ":", columnsWithHorizontalLines[index]);
+
         let sequence = [];
         let currentY = 90; // 시작점의 Y 위치
         let currentColumn = index; // 현재 세로줄 인덱스
 
-        horizontalLines.forEach(line => {
-            if (line.top > currentY && line.top < currentY + 500) {
-                // 현재 위치에서 가로줄까지의 수직 이동 추가
-                sequence.push(
-                    Animated.timing(positions[index].y, {
-                        toValue: line.top,
-                        duration: (line.top - currentY) * 1, // 이동 속도 조정
-                        useNativeDriver: false
-                    })
-                );
 
-                // 수평 이동 추가
-                const targetColumn = line.connectedColumn === currentColumn ? currentColumn + 1 : currentColumn - 1;
-                if (targetColumn >= 0 && targetColumn < cnt) {
-                    sequence.push(
-                        Animated.timing(positions[index].x, {
-                            toValue: targetColumn * columnWidth, // 목표 세로선의 X 위치
-                            duration: 500, // 수평 이동 시간
-                            useNativeDriver: false
-                        })
-                    );
-                    currentColumn = targetColumn; // 현재 세로줄 인덱스 업데이트
-                }
-                currentY = line.top; // 현재 Y 위치 업데이트
-            }
-        });
-
-        // 가장 마지막 수직 이동 추가
-        sequence.push(
-            Animated.timing(positions[index].y, {
-                toValue: 500,
-                duration: (500 - currentY) * 1,
-                useNativeDriver: false
-            })
-        );
+        // 마지막 수직 이동 추가
+        sequence.push(Animated.timing(positions[index].y, {
+            toValue: 500, duration: (500 - currentY) * 1, useNativeDriver: false
+        }));
 
         Animated.sequence(sequence).start();
     };
@@ -76,11 +74,12 @@ const Sadari = ({cnt}) => {
                 height: 10,
                 backgroundColor: 'black',
                 left: columnWidth / 2,
-                top: line.top + 180
+                top: line.yPosition
             }}/>);
         }
         return null;
     };
+
 
     return (<View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 100}}>
         {positions.map((position, i) => (
@@ -89,7 +88,7 @@ const Sadari = ({cnt}) => {
                 <View style={{
                     position: 'absolute',
                     width: 10,
-                    height: 500,
+                    height: 600,
                     backgroundColor: 'black',
                     left: columnWidth / 2 - 5,
                     top: 90
