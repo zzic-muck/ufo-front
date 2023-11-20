c4e8201e5feaef389a4de3dc0208a62b5117988b
zzicmuck <91383940+zzic-muck@users.noreply.github.com>
2023-11-20 18:08:53 +0900

    사다리 뼈대완성

--- components/sadari.js
+++ components/sadari.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Animated, Dimensions, Image, TouchableOpacity, View} from 'react-native';
+import {Animated, Dimensions, Image, TouchableOpacity, View, Text } from 'react-native';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -7,7 +7,8 @@ const Sadari = ({cnt}) => {
     const [positions, setPositions] = useState([]);
     const [horizontalLines, setHorizontalLines] = useState([]);
     const [columnsWithHorizontalLines, setColumnsWithHorizontalLines] = useState([]);
-
+    const [finalIndexes, setFinalIndexes] = useState(Array(cnt).fill(null)); // 각 세로줄의 최종 lineIndex를 저장하는 state
+    const columnWidth = windowWidth / (1.5 * cnt);
     const imageUriArray = ['https://i.namu.wiki/i/NB_qC6YRjH7hv6elNznBIBOBZ5AwE-PKYEWKcU03aFzGsc60bOt9KLxocyvB01OxAbOG8joW9mgkShFmTaTKsQ.webp'];
 
 
@@ -19,7 +20,7 @@ const Sadari = ({cnt}) => {
         const columnsWithHorizontalLines = Array(cnt).fill().map(() => []);
 
         for (let i = 0; i < cnt - 1; i++) {
-            // for (let j = 0; j < 3; j++) { // 각 세로선당 3개의 가로선
+
             const yPosition = Math.random() * 300 + 100;
             newHorizontalLines.push({
                 fromColumn: i, toColumn: i + 1, yPosition: yPosition,
@@ -34,36 +35,86 @@ const Sadari = ({cnt}) => {
             columnsWithHorizontalLines[i + 1].push({
                 fromColumn: i, yPosition: yPosition,
             });
-            // }
-        }
 
+        }
+        console.log(columnsWithHorizontalLines);
         setHorizontalLines(newHorizontalLines);
         // columnsWithHorizontalLines에 각 세로줄별 가로줄 정보가 저장됩니다.
         setColumnsWithHorizontalLines(columnsWithHorizontalLines);
-        console.log(columnsWithHorizontalLines); // 디버깅을 위해 콘솔에 출력
+        for (let j = 0; j < columnsWithHorizontalLines.length; j++) {
+            columnsWithHorizontalLines[j].sort((a, b) => a.yPosition - b.yPosition);
+        }
     }, [cnt]);
 
 
     const moveImage = index => {
-        console.log("Clicked image index:", index); // 클릭한 이미지의 인덱스를 콘솔에 표시
-        // 여기서 columnsWithHorizontalLines 사용 가능
-        console.log("Horizontal lines for column", index, ":", columnsWithHorizontalLines[index]);
 
         let sequence = [];
         let currentY = 90; // 시작점의 Y 위치
-        let currentColumn = index; // 현재 세로줄 인덱스
+        let currentX = 0;
+        let movto = 0;
+        let lineIndex = index;
+
+        for (let i = 0; i < 3; i++) {
+            let nowpos = 0;
+            console.log("반복횟수", i);
+            for (let j = 0; j < columnsWithHorizontalLines[lineIndex].length; j++) {
+                if (columnsWithHorizontalLines[lineIndex][j].yPosition > currentY) {
+                    sequence.push(Animated.timing(positions[index].y, {
+                        toValue: columnsWithHorizontalLines[lineIndex][j].yPosition - 10,
+                        duration: (500 - currentY) * 1,
+                        useNativeDriver: false
+                    }));
+                    currentY = columnsWithHorizontalLines[lineIndex][j].yPosition
+                    console.log("움직였다....");
+                    nowpos = j;
+                    break;
+                }
+            }
+            console.log("지금은 어디인가요 ", currentX);
+
+            //옆으로 가자
+            // 옆으로 이동
+            let line = columnsWithHorizontalLines[lineIndex][nowpos];
+            if (line.toColumn !== undefined && line.toColumn !== lineIndex) {
 
 
-        // 마지막 수직 이동 추가
-        sequence.push(Animated.timing(positions[index].y, {
-            toValue: 500, duration: (500 - currentY) * 1, useNativeDriver: false
-        }));
+                currentX += columnWidth; // 오른쪽으로 이동
+                lineIndex = line.toColumn;
+            } else if (line.fromColumn !== undefined && line.fromColumn !== lineIndex) {
+
+
+                currentX -= columnWidth; // 왼쪽으로 이동
+                lineIndex = line.fromColumn;
+            }
+
+            sequence.push(Animated.timing(positions[index].x, {
+                toValue: currentX, duration: 300, useNativeDriver: false
+            }));
+
+
+            console.log("어디로 갈까요", lineIndex);
+
+            let lastidx = columnsWithHorizontalLines[lineIndex].length - 1;
+
+            if (currentY === columnsWithHorizontalLines[lineIndex][lastidx].yPosition) {
+                console.log("끝난듯??", i);
+                sequence.push(Animated.timing(positions[index].y, {
+                    toValue: 600, duration: (500 - currentY) * 1, useNativeDriver: false
+                }));
+                currentY = 600;
+                break;
+            }
+        }
+        console.log(" 얘 결과 값", lineIndex);
+        setFinalIndexes(prevIndexes => {
+            const newIndexes = [...prevIndexes];
+            newIndexes[index] = lineIndex;
+            return newIndexes;
+        });
 
         Animated.sequence(sequence).start();
     };
-
-
-    const columnWidth = windowWidth / (1.5 * cnt);
 
     const renderHorizontalLine = (index) => {
         const line = horizontalLines[index];
@@ -102,6 +153,14 @@ const Sadari = ({cnt}) => {
                         />
                     </Animated.View>
                 </TouchableOpacity>
+                {finalIndexes[i] !== null && (
+                    <Text style={{ position: 'absolute', bottom: 0, left: columnWidth / 2, color: 'black' }}>
+                        {finalIndexes[i]}
+                    </Text>
+                )}
+                <Text style={{ position: 'absolute', top: 700, left: columnWidth / 2 - 50, color: 'black' }}>
+                    {"여기에 원하는 텍스트"}
+                </Text>
             </View>))}
     </View>);
 };
